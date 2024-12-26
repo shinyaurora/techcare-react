c6be33a1fac14cd5c43ddbcb811fc834d3518800
JackAdams121 <pro.jackadams0121@gmail.com>
2024-12-25 17:00:14 -0800

    step1

---
+++ src/components/patient-detail/detail-info.jsx
@@ -0,0 +1,17 @@
+
+
+const PatiendDetailInfo = ({cate, value}) => {
+
+
+    return (
+        <div>
+            <div>
+                <img />
+            </div>
+            <div>
+                <p>{cate}</p>
+                <p>{value}</p>
+            </div>
+        </div>
+    )
+}
--- src/components/patient-detail/index.jsx
+++ src/components/patient-detail/index.jsx
@@ -6,12 +6,21 @@ const PatientDetail = () => {
         "Gender": "Female",
         "Contact Info.": "(415) 555-1234",
         "Emergency Contacts": "(415) 555-5678",
-        
+        "Insurance Provider": "Sunrise Health Assurance"
     }
 
     return (
         <div>
-
+            <div>
+                <img></img>
+                <p>Jessica Taylor</p>
+            </div>
+            <div>
+                {
+                    Object.entries(data).map((key, value) => <PatiendDetailInfo cate={key} value={value}/>)
+                }
+                <div>Show All Information</div>
+            </div>
         </div>
     )
 }

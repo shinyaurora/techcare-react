dd6ff943b61e1bfac298bc38fd2e74d68f80135b
JackAdams121 <pro.jackadams0121@gmail.com>
2024-12-25 17:01:19 -0800

    pull

--- src/components/diagnostic-list/index.jsx
+++ src/components/diagnostic-list/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import DiagnosticDataList from "./data-list"
 
 const DiagnosticList = () => {
@@ -6,18 +5,8 @@ const DiagnosticList = () => {
         <div className="">
             <p className="">Diagnostic List</p>
             <DiagnosticDataList />
-=======
-const DiagnosticList = () => {
-    return (
-        <div className="min-h-96 w-full rounded-2xl bg-white">
-
->>>>>>> db8110237dce49611884e6f4c831abe9a383ad4b
         </div>
     )
 }
 
-<<<<<<< HEAD
 export default DiagnosticList
-=======
-export default DiagnosticList;
->>>>>>> db8110237dce49611884e6f4c831abe9a383ad4b

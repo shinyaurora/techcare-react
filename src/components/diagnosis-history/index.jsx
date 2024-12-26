cb6852991b4bc2dc4e3843c4b692cef2307b1aeb
JackAdams121 <pro.jackadams0121@gmail.com>
2024-12-25 18:33:55 -0800

    component

--- src/components/diagnosis-history/index.jsx
+++ src/components/diagnosis-history/index.jsx
@@ -1,7 +1,36 @@
+import DetailColorComponent from "./detail-colorcomponent"
+
 const DiagnosisHistory = () => {
+
+    const data = [
+        {
+            cate: "Respiratory Rate",
+            value: "20 bpm",
+            state: "Normal"
+        },
+        {
+            cate: "Temperature",
+            value: "98.6°F",
+            state: "Normal"
+        },
+        {
+            cate: "Heart Rate",
+            value: "78 bpm",
+            state: "Lower than Average"
+        },
+    ]
+
     return (
         <div className="min-h-96 w-full rounded-2xl bg-white">
-
+            
+            <div className="flex">
+            {
+                data.map((ele, key) => 
+                    <DetailColorComponent cate={ele.cate} value={ele.value} state={ele.state} key={key}/>
+                )
+            }
+            </div>
+            
         </div>
     )
 }

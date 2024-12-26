e8eb4061617efec66b7dffb514ef7106d7954dd0
JackAdams121 <pro.jackadams0121@gmail.com>
2024-12-25 17:55:53 -0800

    Diagostic List

--- src/components/diagnostic-list/data-list.jsx
+++ src/components/diagnostic-list/data-list.jsx
@@ -25,16 +25,16 @@ const DiagnosticDataList = () => {
     ]
 
     return (
-        <table>
-            <thead className="">
-                <tr>
-                    <th>
+        <table className="mt-[40px] w-full">
+            <thead>
+                <tr className="h-[48px] text-[14px] font-bold">
+                    <th className="text-left pl-5 rounded-l-[24px] bg-[#F6F7F8]">
                         Problem/Diagnosis
                     </th>
-                    <th>
+                    <th className="text-left pl-5 bg-[#F6F7F8]">
                         Description
                     </th>
-                    <th>
+                    <th className="text-left pl-5 rounded-r-[24px] bg-[#F6F7F8]">
                         Status
                     </th>
                 </tr>
@@ -42,14 +42,14 @@ const DiagnosticDataList = () => {
             <tbody>
                 {
                     data.map((ele, key) => 
-                        <tr className="" key={key}>
-                            <td>
+                        <tr className="border-b text-[14px]" key={key}>
+                            <td className="pl-5 py-3">
                                 {ele.problem}
                             </td>
-                            <td>
+                            <td className="pl-5 py-3">
                                 {ele.description}
                             </td>
-                            <td>
+                            <td className="pl-5 py-3">
                                 {ele.status}
                             </td>
                         </tr>
